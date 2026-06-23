6edf208846d4860561056a7b819e68ae159041e6
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2026-06-24 03:03:45 +0800

    fix(OAuth): token refresh with refreshTokenUrl and bodyEncoding

--- src/api/oauth.ts
+++ src/api/oauth.ts
@@ -13,13 +13,17 @@ type BangumiTokenResponse = Omit<OAuth.TokenResponse, "scope"> & {
   user_id?: number
 }
 
+const tokenUrl =
+  "https://oauth.raycast.com/v1/token/7CSgIYGNn4jFuD6fTG5ov64wsfsi3iegonKLu0FCzW4pWL1T1jgXa4Bxa4f5w-bZOModMtBbrKFPsx1bGT4LUL_pmFzC1O3PkRp4Al1FToLwL_OPG4wVCAnQBQv6d2A"
+
 export const bangumiAuth = new OAuthService({
   client,
   clientId: "bgm62976a25c521a8187",
   authorizeUrl:
     "https://oauth.raycast.com/v1/authorize/nAxnz5SEQ7fO5SRxM_DjKrJ4k3C00kfJCNh1-fp_ZcDFXVcYIxtPBI5BbGux3-EOO8WfSheAkSZ7EJRtTfq1EQkaxB4ZKpkJ9byrPZZfYMx35C1Ii85HfKYDVo4",
-  tokenUrl:
-    "https://oauth.raycast.com/v1/token/7CSgIYGNn4jFuD6fTG5ov64wsfsi3iegonKLu0FCzW4pWL1T1jgXa4Bxa4f5w-bZOModMtBbrKFPsx1bGT4LUL_pmFzC1O3PkRp4Al1FToLwL_OPG4wVCAnQBQv6d2A",
+  tokenUrl,
+  refreshTokenUrl: tokenUrl,
+  bodyEncoding: "url-encoded",
   scope: "",
   tokenResponseParser: (response) => {
     const data = response as BangumiTokenResponse
